4808283518321ea42c3128b27ea30c0e5f8ae60c
= <=>
2025-12-24 12:56:56 +0100

    feat: themeing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "./components/ui/button";
+import { Card } from "./components/ui/card";
 
-function App() {
-  const [count, setCount] = useState(0);
+export function App() {
+  const [response, setResponse] = useState("No request sent.");
 
-  const fetchData = () => {
-    fetch(`${import.meta.env.VITE_API}/`)
-      .then((response) => response.text())
-      .then((data) => setMessage(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  };
-  const [message, setMessage] = useState<string>("");
-
+  function pingBackend() {
+    fetch(`${import.meta.env.VITE_API}/`).then(async (res) => {
+      try {
+        const msg = await res.json();
+        setResponse(msg.message + " - " + res.status);
+      } catch {
+        console.error("Something went wrong");
+      }
+    });
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button onClick={fetchData}>Click to fetch from Go server</button>
-      {message && (
-        <div>
-          <h2>Server Response:</h2>
-          <p>{message}</p>
-        </div>
-      )}
-    </>
+    <div className="h-screen w-screen flex items-center justify-center flex-col gap-4">
+      <h1 className="text-3xl">Go Dashboard</h1>
+      <Button onClick={pingBackend}>Ping Backend</Button>
+      <Card className="p-4">{response}</Card>
+    </div>
   );
 }
 
